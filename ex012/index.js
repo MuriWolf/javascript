8fe5a388a17cc77c1374351bb8ff940c7e3125b6
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-03-15 20:09:54 -0300

    adicao arquiuvos

---
+++ ex012/index.js
@@ -0,0 +1,19 @@
+// menor 16 - nao vota, maior 16 vota, maior 70 - nao obrigatorio
+
+var idade = -1
+
+if (idade < 0) {
+    console.log("Digite uma idade válida!") // idade impossivel
+}
+
+else if (idade < 16) {
+    console.log("Não pode votar!") // não pode votar | crianca
+}
+
+else if ((idade >= 16) && (idade < 70)) {
+    console.log("Voto obrigatório!") // voto obrigatorio | adulto
+}
+
+else {
+    console.log("Voto não obrigatório!") // voto nao obrigatorio | idoso
+}
