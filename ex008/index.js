9d6655803169de2093ed550d7bae1d5784d1ec3c
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-03-15 17:01:52 -0300

    adicao

---
+++ ex008/index.js
@@ -0,0 +1,9 @@
+var vel = 81
+
+if (vel > 80) {
+    console.log("Multado!")
+} else {
+    console.log("Não multado!")
+}
+
+console.log("Digija sempre com cintos de segurança!")
---
+++ ex009/index.js
@@ -0,0 +1,9 @@
+var pais = "Canadá"
+var pais = pais.toLowerCase()
+
+console.log(`Vivendo em ${pais}`)
+if (pais == "brasil") {
+    console.log("Você é brasileiro.")
+} else {
+    console.log("Você é estrangeiro.")
+}
