7dc51ee399e45fb7bfa7720627e4dcd2c9c0099b
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-03-22 21:20:56 -0300

    adicapo_ex

---
+++ ex023/script.js
@@ -0,0 +1,21 @@
+var res = document.getElementById("res")
+var lista_output = document.querySelector("#lista_output")
+var lista_num = []
+var num = document.getElementById("num")
+
+
+function adicionar() {
+    lista_num.push(Number(num.value))
+    alert(lista_num)
+}
+
+function qtde_numeros() {
+
+}
+
+
+function teste() {
+    alert("aspoa")
+}
+
+// DEIXA ABERTO !!
