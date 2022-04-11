a5c1a96a1e0a755d84ceda78792cb80cecf47cc7
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-04-11 01:51:19 -0300

    javascript30

--- teste.js
+++
@@ -1,8 +0,0 @@
-function sortfunction(a, b){
-    return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
-  }
-var Data = [3,6,1,4,5,2,7];
-var b = Data.sort(sortfunction); //resultado: [1, 3, 3, 5, 7, 9, 10]
-
-console.log(b)
-console.log(b[b.length - 1])
