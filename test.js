acf35e4fdfa31f7dcd53394f32b7ec7852a1f773
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-06-15 22:29:57 -0300

    codigo js da pag 17

--- test.js
+++ test.js
@@ -1,9 +1,39 @@
+const cards = document.querySelectorAll(".c-card__content");
+const time1 = document.querySelector(".time1");
+const time2 = document.querySelector(".time2");
+const time3 = document.querySelector(".time3");
 
-const a = 2
-const b = 5
 
+function getJson(time) {
+    fetch("data.json")
+    .then((resp)=> resp.json())
+    .then(function(data) {
 
-for (let c=1; c<30; c++) {
-    const random = Math.floor(Math.random() * 6);
-    console.log(random)
+        let c = 0
+        cards.forEach(card =>{
+            const current = data[c].timeframes[time].current;
+            const previous = data[c].timeframes[time].previous;
+
+            texto1 = card.querySelector(".texto1");
+            texto2 = card.querySelector(".texto2");
+
+            texto1.textContent = current;
+            texto2.textContent = previous;
+            c++
+        })
+    }) 
 }
+
+function daily() {
+    getJson("daily");
+}
+function weekly() {
+    getJson("weekly");
+}
+function monthly() {
+    getJson("monthly");
+}
+
+time1.addEventListener("click", daily);
+time2.addEventListener("click", weekly);
+time3.addEventListener("click", monthly);
