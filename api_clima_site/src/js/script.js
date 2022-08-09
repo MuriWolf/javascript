2af47a407af40015cfcaef60695515e7bd150681
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-08-09 17:39:39 -0300

    update api clima

--- api_clima_site/src/js/script.js
+++ api_clima_site/src/js/script.js
@@ -1,10 +1,26 @@
-const btnSubmit = document.querySelector(".btn-submit");
+const btnSubmit = document.querySelector(".btn-send");
 
 btnSubmit.addEventListener("click", () => {
-    const lon = document.querySelector("#lon").value;
-    const lat = document.querySelector("#lat").value;
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=139&appid=a8964c08f0867f5163d14200e7698d52')
-        .then(response => response.json())
-        .then(response => console.log(response))
-    .catch(err => console.error(err))
+    const city = document.querySelector("#city").value.toLowerCase();
+    const state = document.querySelector("#state").value.toLowerCase();
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=a8964c08f0867f5163d14200e7698d52`)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        response.every(function(local, i) {
+            if(local.state.toLowerCase() == state) {
+                lat = local.lat; 
+                lon = local.lon;
+                return false;
+            } 
+            else return true;
+        })
+        
+        console.log(lat,lon);
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a8964c08f0867f5163d14200e7698d52`)
+            .then(response => response.json())
+            .then(response => console.log(response))
+        .catch(err => console.error(err))
+    })
+
 })
