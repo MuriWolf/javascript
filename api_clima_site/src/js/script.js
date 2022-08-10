f3a696bf2a6e9e3952fabc325eb5646abd5f3b30
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-08-10 17:45:34 -0300

    update

--- api_clima_site/src/js/script.js
+++ api_clima_site/src/js/script.js
@@ -6,50 +6,76 @@ async function fetchAPI(url) {
     return jsonBody;
 }
 
-const estadosNomes = fetchAPI(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`).then(response => {
-    return response.map(estado => estado.nome);
+fetchAPI(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`).then(estados => {
+    estadosSelect = document.querySelector("#estados");
+    estadosSelect.addEventListener("change", getEstado);
+    estadosSelect.innerHTML = estados.map((estado) => {
+        return `
+          <option value='${estado.id}'>
+            ${estado.nome}
+          </option>
+        `;
+      }).join('');
+      getEstado();
 });
 
-estadosNomes.then(estados => {
-    estadosSelect = document.querySelector("#estados")
-    let option = ("<option value='ff'>sao paulo</option>") 
-    estadosSelect.appendChild(option);
-    console.log(estados)
-});
+function getOptionSelectedValue(name) {
+    return name.options[name.selectedIndex].value;
+}
+function getOptionSelected(name) {
+    return name.options[name.selectedIndex];
+}
+
+function getEstado() {
+    const estadoSeleciondo = getOptionSelectedValue(estadosSelect);
+    showCidades(estadoSeleciondo);
+    return estadoSeleciondo;
+}
+
+function getNameEstado() {
+    const estadoSeleciondo = getOptionSelected(estadosSelect);
+    return estadoSeleciondo.innerHTML;
+}
 
 
+function showCidades(estadoSeleciondo) {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSeleciondo}/municipios`)
+    .then(response => response.json())
+    .then(cidades => {
+        cidadesSelect = document.querySelector("#cidades");
+        cidadesSelect.innerHTML = cidades.map((cidade) => {
+            return `
+              <option value='${cidade.nome}'>
+                ${cidade.nome}
+              </option>
+            `;
+          }).join('');
+    })
+}
 
+function getCidade() {
+    const cidadeSelecionada = getOptionSelectedValue(cidadesSelect);
+    return cidadeSelecionada;
+}
 
-const idEstado = 35;
-
-// // cidade baseada no id selecionado do estado
-// fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${idEstado}/municipios`)
-// .then(response => response.json())
-// .then(response => {
-//     console.log(response)
-// })
-
-// btnSubmit.addEventListener("click", () => {
-//     const city = document.querySelector("#city").value.toLowerCase();
-//     const state = document.querySelector("#state").value.toLowerCase();
-//     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=a8964c08f0867f5163d14200e7698d52`)
-//     .then(response => response.json())
-//     .then(response => {
-//         console.log(response);
-//         response.every(function(local, i) {
-//             if(local.state.toLowerCase() == state) {
-//                 lat = local.lat; 
-//                 lon = local.lon;
-//                 return false;
-//             } 
-//             else return true;
-//         })
+btnSubmit.addEventListener("click", () => {
+    const city = getCidade();
+    const state = getNameEstado();
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=a8964c08f0867f5163d14200e7698d52`)
+    .then(response => response.json())
+    .then(response => {
+        response.every(function(local, i) {
+            if(local.state.toLowerCase() == state) {
+                lat = local.lat; 
+                lon = local.lon;
+                return false;
+            } 
+            else return true;
+        })
         
-//         console.log(lat,lon);
-//         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a8964c08f0867f5163d14200e7698d52`)
-//             .then(response => response.json())
-//             .then(response => console.log(response))
-//         .catch(err => console.error(err))
-//     })
-
-// })
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a8964c08f0867f5163d14200e7698d52`)
+            .then(response => response.json())
+            .then(response => console.log(response))
+        .catch(err => console.error(err))
+    })
+})
